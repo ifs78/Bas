f6c68043ca20a5df4f03a3bf26d2b5678ceadefc
ilshat Sadykov <ifs@front.ru>
2018-08-11 21:19:53 +0500

    Оребренные трубы Тест: Запись в базу MySQL, чтение из базы

--- src/gb/Link.java
+++ src/gb/Link.java
@@ -1,4 +1,7 @@
 package gb;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,21 +13,44 @@ public class Link {
     private static Statement stmt;
     private static ResultSet rs;
     private static String url = "jdbc:mysql://localhost:3306/fintube?autoReconnect=true&useSSL=false";
+    public static String m, d;
 
        public static void main(String[] args) {
-           new TubeForm();
-          try {
-             queryData("insert into material (mark, density) VALUES ('X0808', '1.78')");
-             queryData("select id, mark, density from material");
-              while (rs.next()) getDataDB();
-             System.out.println("Данные из базы получены");
-       } catch (SQLException sqlEx){
-           sqlEx.printStackTrace();
-       } finally {
-          try { con.close(); } catch(SQLException se) { se.getMessage();}
-          try { stmt.close(); } catch(SQLException se) { se.getMessage();}
-          try { rs.close(); } catch(SQLException se) { se.getMessage();}
-       }
+           TubeForm tb = new TubeForm();
+
+           tb.setContentPane(tb.panel1);
+           tb.setBounds(0,0,900,600);
+           tb.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+           tb.setVisible(true);
+
+
+           tb.button1.addActionListener(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                   m = tb.textField1.getText();
+                   d = tb.textField2.getText();
+
+                   try {
+                       queryData("SET NAMES 'utf8'");
+                       queryData("SET CHARACTER SET 'utf8'");
+                       queryData("insert into material (mark, density) VALUES ('" + m  + "', '" + d + "')");
+                       queryData("select id, mark, density from material");
+                       while (rs.next()) getDataDB();
+                       System.out.println("Данные из базы получены");
+                   } catch (SQLException sqlEx){
+                       sqlEx.printStackTrace();
+                   } finally {
+                       try { con.close(); } catch(SQLException se) { se.getMessage();}
+                       try { stmt.close(); } catch(SQLException se) { se.getMessage();}
+                       try { rs.close(); } catch(SQLException se) { se.getMessage();}
+                   }
+
+               }
+           });
+
+          System.out.println(m + d);
+
+
 
     }
     private static void  queryData(String query) throws SQLException  {
--- src/gb/TubeForm.java
+++ src/gb/TubeForm.java
@@ -7,31 +7,17 @@ import java.awt.event.ActionListener;
 import java.util.EventListener;
 
 public class TubeForm extends JFrame {
-    private JPanel panel1;
-    private JButton button1 = new JButton("Press");
-    private JTextField textField1 = new JTextField();
-    private JTextField textField2 = new JTextField();
+    private String m,d;
+    public JPanel panel1;
+    public JButton button1;
+    public JTextField textField1;
+    public JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
 
 
     public TubeForm() {
-        setContentPane(panel1);
-        setBounds(0,0,900,600);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String s = textField1.getText();
-                textField2.setText(s);
-            }
-        });
-
-
 
     }
-
-
 
     }
